809800920a1888f60acc674f46b25462e32232b3
Администратор <a2driano@gmail.com>
2016-02-06 21:59:49 +0200

    add function, when empty textarea, '' don`t add to the chat

--- src/main/webapp/resources/js/onstart_chat.js
+++ src/main/webapp/resources/js/onstart_chat.js
@@ -105,74 +105,78 @@ var onStart = function () {
     });
 };
 
-
 function AjaxFormRequest() {
-    var data = {
-        textForm: $(".textForm").val(),
-        datatime: new Date()
-    };
-    $.ajax({
-        url: $hostRoot + "messageadd",
-        type: 'post',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify(data),
-        success: function (messageHistoryDTO) {
-            if (messageHistoryDTO != null) {
-                $(".textForm").val('');
-                document.getElementById('cform').focus();
-                var today = new Date();
-                var todaydd = today.getDate();
-                var todaymm = today.getMonth() + 1;
-                var todayyyyy = today.getFullYear();
-                if (todaydd < 10) {
-                    todaydd = '0' + todaydd
-                }
-                if (todaymm < 10) {
-                    todaymm = '0' + todaymm
-                }
+    var textarea = document.getElementById('cform').value;
+    if (textarea != '') {
+        var data = {
+            textForm: $(".textForm").val(),
+            datatime: new Date()
+        };
+        $.ajax({
+            url: $hostRoot + "messageadd",
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            success: function (messageHistoryDTO) {
+                if (messageHistoryDTO != null) {
+                    $(".textForm").val('');
+                    document.getElementById('cform').focus();
+                    var today = new Date();
+                    var todaydd = today.getDate();
+                    var todaymm = today.getMonth() + 1;
+                    var todayyyyy = today.getFullYear();
+                    if (todaydd < 10) {
+                        todaydd = '0' + todaydd
+                    }
+                    if (todaymm < 10) {
+                        todaymm = '0' + todaymm
+                    }
 
-                var datatime = new Date(messageHistoryDTO.datatime);
-                var dd = datatime.getDate();
-                var mm = datatime.getMonth() + 1;
-                var yyyy = datatime.getFullYear();
-                var hh = datatime.getHours();
-                var min = datatime.getMinutes();
-                if (dd < 10) {
-                    dd = '0' + dd;
-                }
-                if (mm < 10) {
-                    mm = '0' + mm;
-                }
-                if (hh < 10) {
-                    hh = '0' + hh;
-                }
-                if (min < 10) {
-                    min = '0' + min;
-                }
+                    var datatime = new Date(messageHistoryDTO.datatime);
+                    var dd = datatime.getDate();
+                    var mm = datatime.getMonth() + 1;
+                    var yyyy = datatime.getFullYear();
+                    var hh = datatime.getHours();
+                    var min = datatime.getMinutes();
+                    if (dd < 10) {
+                        dd = '0' + dd;
+                    }
+                    if (mm < 10) {
+                        mm = '0' + mm;
+                    }
+                    if (hh < 10) {
+                        hh = '0' + hh;
+                    }
+                    if (min < 10) {
+                        min = '0' + min;
+                    }
 
-                if ((todaydd + todaymm + todayyyyy) == (dd + mm + yyyy)) {
-                    var time = hh + ':' + min;
+                    if ((todaydd + todaymm + todayyyyy) == (dd + mm + yyyy)) {
+                        var time = hh + ':' + min;
+                    } else {
+                        var time = dd + '/' + mm + '/' + yyyy;
+                    }
+                    var name = messageHistoryDTO.name;
+                    var textForm = messageHistoryDTO.textForm;
+                    $('.chat').append('<div class="messageBlock"><span class="messageName">' + name +
+                        '</span><span class="messageDate">' + time +
+                        '</span><br><span class="messageText">' + textForm + '</span></div>');
+
+                    //scroll chat window in bottom
+                    var objDiv = document.getElementById("chatScroll");
+                    objDiv.scrollTop = objDiv.scrollHeight;
                 } else {
-                    var time = dd + '/' + mm + '/' + yyyy;
+                    console.log("EMPTY DATA");
                 }
-                var name = messageHistoryDTO.name;
-                var textForm = messageHistoryDTO.textForm;
-                $('.chat').append('<div class="messageBlock"><span class="messageName">' + name +
-                    '</span><span class="messageDate">' + time +
-                    '</span><br><span class="messageText">' + textForm + '</span></div>');
-
-                //scroll chat window in bottom
-                var objDiv = document.getElementById("chatScroll");
-                objDiv.scrollTop = objDiv.scrollHeight;
-            } else {
-                console.log("EMPTY DATA");
+            },
+            error: function (error) {
+                console.log("ERROR");
             }
-        },
-        error: function (error) {
-            console.log("ERROR");
-        }
-    });
+        });
+    } else {
+        console.log('empty textarea, need text');
+    }
 };
 
 
